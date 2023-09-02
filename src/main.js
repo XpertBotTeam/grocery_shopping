edeb2df3f8a471b9c3b631617d8b56f7a8a761a6
marwassaid <marwasaid0310@gmail.com>
2023-09-01 23:49:15 -0700

    import vuelidate

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuelidate from 'vuelidate'
+
+Vuelidate.use(Vuelidate)
 
 createApp(App).use(router).mount('#app')
