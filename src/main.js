63c7227570c1a6b3ef9687c7721a7cdc881ea228
marwassaid <marwasaid0310@gmail.com>
2023-09-03 19:36:25 -0700

    install json server

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Vuelidate from 'vuelidate'
 
-Vuelidate.use(Vuelidate)
 
 createApp(App).use(router).mount('#app')
